1cd27fc4b4a18c2a62d0a6824890fe42a62aad67
jackbo11 <bottomleyjm@cardiff.ac.uk>
2016-10-13 10:53:11 +0100

    Don't break plz

--- newplayer.py
+++ newplayer.py
@@ -1,14 +1,29 @@
 class player:
 	def __init__(self, name, gender, age, smoker):
-		self.name = name
-		self.gender = gender
-		self.age = age
-		self.smoker = smoker
-		self.money = 30.0
-		self.drunkenness = 0
-		self.drink_left = 0
-		self.happiness = 1
-		self.cig_left = 0
-		if self.smoker:
-			self.cig_left = 10
+		self.__name = name.title()
+		self.__gender = gender.title()
+		self.__age = age
+		self.__smoker = bool(smoker)
+		self.__money = 30.0
+		self.__drunkenness = 0
+		self.__drink_left = 0
+		self.__happiness = 1
+		self.__cig_left = 0
+		if self.__smoker:
+			self.__cig_left = 10
 		
+	@property
+	def name(self):
+		return self.__name
+
+	@name.setter
+	def name(self, name):
+		print("You cannot change your name!")
+
+    @property
+	def gender(self):
+		return self.__gender
+
+	@gender.setter
+	def gender(self, gender):
+		print("You cannot change your gender!")
