5b8be6ddacfc85752b74764cf6c7cc30c6e4780e
jackbo11 <jack@pbottomley.plus.com>
2016-10-14 11:17:03 +0100

    Put ring of fire into a function

--- ring_of_fire.py
+++ ring_of_fire.py
@@ -5,144 +5,142 @@
 import random
 
 
+def play_ring_of_fire():
+    gen = 0
+    choose = 0
 
-
-
-gen = 0
-choose = 0
-
-print("What is your name?")
-name = input(">>> ")
-print("\n")
-
-print("What is your gender? (m/f)")
-gender = input(">>> ")
-while int(gen) == 0:
-    if str(gender) == "m":
-        gen = 1
-    elif str(gender) == "f":
-        gen = 2
-    else:
-        print("What?")
-        gender = input(">>> ")
+    print("What is your name?")
+    name = input(">>> ")
     print("\n")
 
-inv = {"turns": 0, "drunk": 0}
+    print("What is your gender? (m/f)")
+    gender = input(">>> ")
+    while int(gen) == 0:
+        if str(gender) == "m":
+            gen = 1
+        elif str(gender) == "f":
+            gen = 2
+        else:
+            print("What?")
+            gender = input(">>> ")
+        print("\n")
 
-players = {str(name), "Mia", "Sophie", "Simon", "Juan"}
+    inv = {"turns": 0, "drunk": 0}
 
-def is_player_valid(players, choice):
-    if choice in players:
-        return True
-    else:
-        return False
+    players = {str(name), "Mia", "Sophie", "Simon", "Juan"}
 
-
-print("You start the night by playing ring of fire with your flat mates:")
-print(*players)
-print("\n")
+    def is_player_valid(players, choice):
+        if choice in players:
+            return True
+        else:
+            return False
 
 
+    print("You start the night by playing ring of fire with your flat mates:")
+    print(*players)
+    print("\n")
 
 
 
 
-while int(inv["turns"]) < 20:
-    # if int(inv["turns"]) % 5 == 0:
-    # ^^ commented as I haven't done the code for other players yet
-        print("It is your turn. Press any key to pick a card!")
-        input("")
-        card = random.randrange(1, 13)
-        # card = 2
-        #^^ uncomment and change value to test different card values
-        # A (works)
-        if int(card) == 1:
-            print("You picked an Ace! Everyone drinks.")
-            inv["turns"] = inv["turns"] + 1
-            inv["drunk"] = inv["drunk"] + 1
-        # 2 (broken)
-        elif int(card) == 2:
-            print("2: You! Pick someone else to drink.")
-            choice = input(">>> ")
-            for choice in players:
-                while int(choose) == 0:
-                    if is_player_valid(players, choice):
-                        print(str(choice), "took a drink")
-                        choose = 1
-                        inv["turns"] = inv["turns"] + 1
-                    else:
-                        print("Who?")
-                        choice = input(">>> ")
-        # 3 (works)
-        elif int(card) == 3:
-            print("3: Me! Take a drink.")
-            inv["turns"] = inv["turns"] + 1
-            inv["drunk"] = inv["drunk"] + 1
-        # 4 (works)
-        elif int(card) == 4:
-            if int(gen) == 2:
-                print("4: Whores! Take a drink")
+
+
+    while int(inv["turns"]) < 20:
+        # if int(inv["turns"]) % 5 == 0:
+        # ^^ commented as I haven't done the code for other players yet
+            print("It is your turn. Press any key to pick a card!")
+            input("")
+            card = random.randrange(1, 13)
+            # card = 2
+            #^^ uncomment and change value to test different card values
+            # A (works)
+            if int(card) == 1:
+                print("You picked an Ace! Everyone drinks.")
                 inv["turns"] = inv["turns"] + 1
                 inv["drunk"] = inv["drunk"] + 1
-            else:
-                print("4: Whores! You don't have to drink")
-                inv["turns"] = inv["turns"] + 1
-        # 5 (works)
-        elif int(card) == 5:
-            choose = input("Do you remember rule 5? ")
-            if str(choose) == "thumb":
-                print("Well done, you don't have to drink")
-                inv["turns"] = inv["turns"] + 1
-            else:
-                print("5: Thumbs! Everyone else places their thumb on the table before you. Take a drink")
-                inv["turns"] = inv["turns"] + 1
-                inv["drunk"] = inv["drunk"] + 1
-        # 6 (works)
-        elif int(card) == 6:
-            if int(gen) == 1:
-                print("4: Dicks! Take a drink")
-                inv["turns"] = inv["turns"] + 1
-                inv["drunk"] = inv["drunk"] + 1
-            else:
-                print("4: Dicks! You don't have to drink")
-                inv["turns"] = inv["turns"] + 1
-        # 7 (works)
-        elif int(card) == 7:
-            choose = input("Do you remember rule 7? ")
-            if str(choose) == "heaven":
-                print("Well done, you don't have to drink")
-                inv["turns"] = inv["turns"] + 1
-            else:
-                print("7: Heaven! Everyone else puts their hand in the air before you. Take a drink")
+            # 2 (broken)
+            elif int(card) == 2:
+                print("2: You! Pick someone else to drink.")
+                choice = input(">>> ")
+                for choice in players:
+                    while int(choose) == 0:
+                        if is_player_valid(players, choice):
+                            print(str(choice), "took a drink")
+                            choose = 1
+                            inv["turns"] = inv["turns"] + 1
+                        else:
+                            print("Who?")
+                            choice = input(">>> ")
+            # 3 (works)
+            elif int(card) == 3:
+                print("3: Me! Take a drink.")
                 inv["turns"] = inv["turns"] + 1
                 inv["drunk"] = inv["drunk"] + 1
-        # 8 (broken)
-        elif int(card) == 8:
-            choice = input("8: Mate! Pick someone else to drink with.")
-            for value in players.items():
-                while int(choose) == 0:
-                    if str(choice) == value:
-                        print(value, "took a drink")
-                        choose = 1
-                        inv["turns"] = inv["turns"] + 1
-                        inv["drunk"] = inv["drunk"] + 1
-                    else:
-                        print("who?")
-        # 9 (uncompleted)
-        elif int(card) == 9:
-            print("")
-        # 10 (uncompleted)
-        elif int(card) == 10:
-            print("")
-        # J (uncompleted)
-        elif int(card) == 11:
-            print("")
-        # Q (uncompleted)
-        elif int(card) == 12:
-            print("")
-        # K (works)
-        elif int(card) == 13:
-            print("Oh no! It's a king! Drink the dirty pint")
-            inv["turns"] = inv["turns"] + 1
-            inv["drunk"] = inv["drunk"] + 5
-        choose = 0
+            # 4 (works)
+            elif int(card) == 4:
+                if int(gen) == 2:
+                    print("4: Whores! Take a drink")
+                    inv["turns"] = inv["turns"] + 1
+                    inv["drunk"] = inv["drunk"] + 1
+                else:
+                    print("4: Whores! You don't have to drink")
+                    inv["turns"] = inv["turns"] + 1
+            # 5 (works)
+            elif int(card) == 5:
+                choose = input("Do you remember rule 5? ")
+                if str(choose) == "thumb":
+                    print("Well done, you don't have to drink")
+                    inv["turns"] = inv["turns"] + 1
+                else:
+                    print("5: Thumbs! Everyone else places their thumb on the table before you. Take a drink")
+                    inv["turns"] = inv["turns"] + 1
+                    inv["drunk"] = inv["drunk"] + 1
+            # 6 (works)
+            elif int(card) == 6:
+                if int(gen) == 1:
+                    print("4: Dicks! Take a drink")
+                    inv["turns"] = inv["turns"] + 1
+                    inv["drunk"] = inv["drunk"] + 1
+                else:
+                    print("4: Dicks! You don't have to drink")
+                    inv["turns"] = inv["turns"] + 1
+            # 7 (works)
+            elif int(card) == 7:
+                choose = input("Do you remember rule 7? ")
+                if str(choose) == "heaven":
+                    print("Well done, you don't have to drink")
+                    inv["turns"] = inv["turns"] + 1
+                else:
+                    print("7: Heaven! Everyone else puts their hand in the air before you. Take a drink")
+                    inv["turns"] = inv["turns"] + 1
+                    inv["drunk"] = inv["drunk"] + 1
+            # 8 (broken)
+            elif int(card) == 8:
+                choice = input("8: Mate! Pick someone else to drink with.")
+                for value in players.items():
+                    while int(choose) == 0:
+                        if str(choice) == value:
+                            print(value, "took a drink")
+                            choose = 1
+                            inv["turns"] = inv["turns"] + 1
+                            inv["drunk"] = inv["drunk"] + 1
+                        else:
+                            print("who?")
+            # 9 (uncompleted)
+            elif int(card) == 9:
+                print("")
+            # 10 (uncompleted)
+            elif int(card) == 10:
+                print("")
+            # J (uncompleted)
+            elif int(card) == 11:
+                print("")
+            # Q (uncompleted)
+            elif int(card) == 12:
+                print("")
+            # K (works)
+            elif int(card) == 13:
+                print("Oh no! It's a king! Drink the dirty pint")
+                inv["turns"] = inv["turns"] + 1
+                inv["drunk"] = inv["drunk"] + 5
+            choose = 0
