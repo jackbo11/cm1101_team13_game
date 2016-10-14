eac56ad352095fe6d2edd4f3e9c89185b9417a1d
jackbo11 <bottomleyjm@cardiff.ac.uk>
2016-10-14 11:08:15 +0100

    Change start room

--- player.py
+++ player.py
@@ -4,4 +4,4 @@ from map import rooms
 inventory = [item_id, item_driving_license, item_playing_cards, item_money]
 
 # Start game at the outside
-current_room = rooms["Outside"]
+current_room = rooms["Talybont"]
