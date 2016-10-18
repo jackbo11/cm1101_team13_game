fc8f13542541ca851fa94c496c32f6c47ac728b3
christianman <RusanovKP@cardiff.ac.uk>
2016-10-18 11:58:30 +0100

    Add files via upload

---
+++ drinks.py
@@ -0,0 +1,74 @@
+drink_single = {
+    "id": "single",
+
+    "name": "A single",
+
+    "description": "balh blah"
+
+    "drunkenness_effect": 3
+
+    "price": 2
+
+}
+
+
+
+
+drink_double = {
+    "id": "double",
+
+    "name": "A double",
+
+    "description": "balh blah"
+
+    "drunkenness_effect": 5
+
+    "price": 3
+}
+
+
+
+drink_water = {
+    "id": "single",
+
+    "name": "A single",
+
+    "description": "balh blah"
+
+    "drunkenness_effect": -1
+
+    "price": 0
+
+}
+
+
+
+
+drink_beer = {
+    "id": "beer",
+
+    "name": "A beer",
+
+    "description": "balh blah"
+
+    "drunkenness_effect": 4
+
+    "price": 3
+}
+
+
+
+
+drink_VK = {
+    "id": "VK",
+
+    "name": "A VK",
+
+    "description": "balh blah"
+
+    "drunkenness_effect": 3
+
+    "price": 3
+}
+
+
