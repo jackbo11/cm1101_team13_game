8ce64538e72b8e6e724f315fcf51853ea18287e3
christianman <RusanovKP@cardiff.ac.uk>
2016-10-18 12:00:25 +0300

    Add files via upload

---
+++ flatitems.py
@@ -0,0 +1,21 @@
+flat_item_id = {
+	"name": "id"
+}
+flat_item_wallet = {
+	"name": "wallet"
+}
+flat_item_money = {
+	"name": "money"
+}
+flat_item_phone = {
+	"name": "phone"
+}
+flat_item_cigarettes = {
+	"name": "cigarettes"
+}
+flat_item_predrink = {
+	"name": "wine"
+}
+flat_item_clowndetector = {
+	"name": "clown detector"
+	}
