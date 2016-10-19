bc18003421eb632fe5622608ec8306751a3dcdbc
christianman <RusanovKP@cardiff.ac.uk>
2016-10-19 11:58:53 +0100

    Update map.py

--- map.py
+++ map.py
@@ -108,7 +108,7 @@ room_food = {
     "name": "Food",
 
     "description":
-    """You smell the yummy food. Would you like to get a burger for £3? You can go south to the Dancefloor or eat a burger.""",
+    """You smell the yummy food. You can get a legit subway, burger or chips. You can go south to the Dancefloor or eat a burger.""",
 
     "exits": {"south": "Dancefloor"},
     "time_cost": {"south": 4},
