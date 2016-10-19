670400fcc6af0433bdf612fd994903297a800fd9
jackbo11 <bottomleyjm@cardiff.ac.uk>
2016-10-19 11:06:15 +0100

    Add a sound tester.
    If you want to test sounds, change sound = to the name of the sound you
    want to test, then run: python soundtester.py

---
+++ soundtester.py
@@ -0,0 +1,11 @@
+from sounds import *
+from soundplayer import SoundPlayer
+def main():
+    sound = scales
+    SoundPlayer.play_beep(sound)
+
+# Are we being run as a script? If so, run main().
+# '__main__' is the name of the scope in which top-level code executes.
+# See https://docs.python.org/3.4/library/__main__.html for explanation
+if __name__ == "__main__":
+    main()
